750b3eb8c5a14dd6692dc87b543a02f2849f9840
Владислав <shilovladshilo@gmail.com>
2016-06-29 01:37:31 +0300

    portfolio
    delete old project and add new

---
+++ js/common.js
@@ -0,0 +1,81 @@
+(function(){
+    
+    var app = {
+        
+        init : function(){
+            this.setUpListeners();
+        },
+        
+        setUpListeners : function(){
+            $('form').on('submit', app.submitForm);
+            $('form').on('keydown', 'input', app.removeError);
+        },
+        
+        submitForm : function(e){
+            e.preventDefault();
+            
+            var form = $(this),
+                submitBtn = form.find('button[type="submit"]');
+            
+            if( app.validateForm(form) === false ) return false;
+            
+            submitBtn.attr('disabled', 'disabled');
+            
+            var str = form.serialize();
+            
+            $.post("ajax/form.php", str, function(data){
+                
+                if(data.status == 'success'){
+                    form.html( data.text );
+                }else{
+                    form.html( data.text );
+                }
+                
+                submitBtn.removeAttr('disabled');
+                
+            }, "JSON");
+            
+            console.log('go in ajax');
+        },
+        
+        validateForm : function(form){
+            var inputs = form.find('input'),
+                valid = true;
+            
+            inputs.tooltip('destroy');
+            
+            $.each(inputs, function(index, val){
+                var input     = $(val),
+                    val       = input.val(),
+                    formGroup = input.parents('.form-group'),
+                    label     = formGroup.find('label').text().toLowerCase(),
+                    textError = 'Введите ' + label;
+                
+                if(val.length === 0){
+                    
+                    formGroup.addClass('has-error').removeClass('has-success');
+                    input.tooltip({
+                        trigger   : 'manual',
+                        placement : 'right',
+                        title     : textError
+                    }).tooltip('show');
+                    valid = false;
+                    
+                }else{
+                    formGroup.addClass('has-success').removeClass('has-error');
+                }
+                
+                    
+            });
+            return valid;
+        },
+        
+        removeError : function(){
+            $(this).tooltip('destroy').parents('form-froup').removeClass('has-error');
+        }
+        
+    }
+    
+    app.init();
+    
+}());
